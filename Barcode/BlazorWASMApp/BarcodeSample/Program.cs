d91d4792de59555f3add10db077d8b6186ecde5d
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2022-01-31 16:39:09 +0530

    Added getting started sample for Blazor components

---
+++ Barcode/BlazorWASMApp/BarcodeSample/Program.cs
@@ -0,0 +1,14 @@
+using BarcodeSample;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTc0Mzk2QDMxMzkyZTM0MmUzMGxqN2wrdVR1RlRFYnZtNTFIVldsV1Q1VkdONzFxRU5zZzlZOHJkNUR0SzA9");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
+
+await builder.Build().RunAsync();
