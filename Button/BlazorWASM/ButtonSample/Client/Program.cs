b85c54efa2c454cb54396526d8d9c41bddf726c6
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2022-02-01 15:34:59 +0530

    Modified the Button component WASM Sample

--- Button/BlazorWASM/ButtonSample/Client/Program.cs
+++ Button/BlazorWASM/ButtonSample/Client/Program.cs
@@ -1,4 +1,4 @@
-using ButtonSample;
+using ButtonSample.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Syncfusion.Blazor;
@@ -6,7 +6,7 @@ using Syncfusion.Blazor;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTc0Mzk2QDMxMzkyZTM0MmUzMGxqN2wrdVR1RlRFYnZtNTFIVldsV1Q1VkdONzFxRU5zZzlZOHJkNUR0SzA9");
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTc0OTkyQDMxMzkyZTM0MmUzMGxqN2wrdVR1RlRFYnZtNTFIVldsV1Q1VkdONzFxRU5zZzlZOHJkNUR0SzA9");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
