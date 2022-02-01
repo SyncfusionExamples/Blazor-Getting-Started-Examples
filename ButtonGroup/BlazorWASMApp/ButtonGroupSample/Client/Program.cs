fbb46547507ef007d54663dd0a1e76cf877a7dd4
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2022-02-01 14:41:36 +0530

    Added samples for Getting Started document in Blazor components

---
+++ ButtonGroup/BlazorWASMApp/ButtonGroupSample/Client/Program.cs
@@ -0,0 +1,14 @@
+using ButtonGroupSample.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTc0OTkyQDMxMzkyZTM0MmUzMGxqN2wrdVR1RlRFYnZtNTFIVldsV1Q1VkdONzFxRU5zZzlZOHJkNUR0SzA9");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
+
+await builder.Build().RunAsync();
---
+++ Chart/BlazorServerApp/ChartSample/Program.cs
@@ -0,0 +1,34 @@
+using ChartSample.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Syncfusion.Blazor;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTc0OTkyQDMxMzkyZTM0MmUzMGxqN2wrdVR1RlRFYnZtNTFIVldsV1Q1VkdONzFxRU5zZzlZOHJkNUR0SzA9");
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
