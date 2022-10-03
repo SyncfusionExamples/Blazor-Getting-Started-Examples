f13ef198e8f41dc768ff0653aca82ae091cbe260
Vigneshwaran-SF3888 <vigneshwaran.sf3888@gmail.com>
2022-10-03 20:20:38 +0530

    samples(BLAZ-25684): FAB, Speed dial, Skeleton samples added.

---
+++ FloatingActionButton/BlazorWASM/FABSample/Program.cs
@@ -0,0 +1,12 @@
+using FABSample;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+await builder.Build().RunAsync();
