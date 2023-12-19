91963c138da52e21b4b4c5c4d1ec326762c00f44
sumathiuthayakumar <sumathi.uthayakumar@syncfusion.com>
2023-12-19 18:42:30 +0530

    ES-849370-Sample added

---
+++ DiagramComponent/BlazorMAUIApp/DiagramSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DiagramSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ DiagramComponent/BlazorMAUIApp/DiagramSample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DiagramSample
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
