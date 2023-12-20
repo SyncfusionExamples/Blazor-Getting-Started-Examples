c61ac347a87049166aed4d3ffebb54dfd99cec75
Sathiskumar KR <sathiskumar.rajendran@syncfusion.com>
2023-12-20 16:24:40 +0530

    Sample(Sample_DataForm): Create the sample for getting started with Blazor Data form and Stepper

---
+++ DataForm/BlazorWASMApp/DataForm_Wasm/Program.cs
@@ -0,0 +1,13 @@
+using DataForm_Wasm;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSyncfusionBlazor();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
