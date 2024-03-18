adb52b28939c5157f0066abf2f03e4daed0fc53d
Sathiskumar KR <sathiskumar.rajendran@syncfusion.com>
2024-03-18 16:19:17 +0530

    Sample(Timeline): Create the Blazor Timeline sample

---
+++ Timeline/BlazorWASMApp/Timeline_Wasm/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Timeline_Wasm;
+using Syncfusion.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSyncfusionBlazor();
+
+await builder.Build().RunAsync();
