dbf5aa345523f16b0be2d5f7cdc70e39a0ff6af8
AthiswaranVeerasekaran <athiswaran.v@syncfusion.com>
2025-12-08 23:37:36 +0530

    Migrated all projects to latest TFM and Updated NuGet Packages

---
+++ RangeSelector/NET8/RangeSelector_WasmApp/RangeSelector_WasmApp/Program.cs
@@ -0,0 +1,35 @@
+using RangeSelector_WasmApp.Client.Pages;
+using RangeSelector_WasmApp.Components;
+using Syncfusion.Blazor;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddSyncfusionBlazor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(RangeSelector_WasmApp.Client._Imports).Assembly);
+
+app.Run();
