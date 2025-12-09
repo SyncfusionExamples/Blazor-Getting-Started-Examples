fdd1464fdd366c22f3e0be8bb6f8783b894d4f11
AthiswaranVeerasekaran <athiswaran.v@syncfusion.com>
2025-12-09 10:05:13 +0530

    Migrated all projects to latest TFM and Updated NuGet Packages

---
+++ Skeleton/NET8/Skeleton_WebApp/Skeleton_WebApp/Program.cs
@@ -0,0 +1,37 @@
+using Skeleton_WebApp.Client.Pages;
+using Skeleton_WebApp.Components;
+using Syncfusion.Blazor;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddSyncfusionBlazor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Skeleton_WebApp.Client._Imports).Assembly);
+
+app.Run();
