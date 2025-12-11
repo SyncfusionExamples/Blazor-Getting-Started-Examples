2bfb1b6fee84f1939c0e908f88a8bb75ac4e3182
KarthikeyanSF4227 <150327068+KarthikeyanSF4227@users.noreply.github.com>
2025-12-12 01:33:14 +0530

    786504: Migrated all projects to latest TFM and Updated NuGet Packages

--- PDFViewer  2/BlazorWebApp/WebApp-Wasm/PdfViewerSample.Client/Program.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Syncfusion.Blazor;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.Services.AddMemoryCache();
-//Add Syncfusion Blazor service to the container
-builder.Services.AddSyncfusionBlazor();
-await builder.Build().RunAsync();
