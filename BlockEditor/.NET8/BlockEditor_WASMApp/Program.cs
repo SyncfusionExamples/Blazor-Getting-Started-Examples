7034d13145df005cc78799e8ff2ed5d1e5ff50df
Backiaraj <backiaraj.kanagaraj@syncfusion.com>
2025-12-19 13:18:32 +0530

    Added Blazor Block Editor samples

---
+++ BlockEditor/.NET8/BlockEditor_WASMApp/Program.cs
@@ -0,0 +1,13 @@
+using BlockEditor_WASMApp;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSyncfusionBlazor();
+
+await builder.Build().RunAsync();
